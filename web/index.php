aaee7782280bc0e9e9c1e58e7e6015d27c94a258
David Zuelke <dzuelke@gmail.com>
2014-05-10 15:54:58 +0200

    use bower for bootstrap dep

---
+++ web/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Heroku Multipack Test</title>
+
+    <!-- Bootstrap -->
+    <link href="vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="container">
+    <h1>Hello, world! <small>PHP + Node.js on Heroku</small></h1>
+    
+    <p>Running <span class="text-info">PHP/<?=PHP_VERSION?></span> on Heroku using the <a href="https://github.com/heroku/heroku-buildpack-php">standard PHP buildpack</a>, serving a site using Bootstrap which was downloaded by Bower, which in turn was installed by the <a href="https://github.com/heroku/heroku-buildpack-nodejs">standard Node.js buildpack</a>.</p>
+    
+    <p>Both put together using <a href="https://github.com/ddollar/heroku-buildpack-multi">ddollar/heroku-buildpack-multi</a>.</p>
+    
+  </body>
+</html>
